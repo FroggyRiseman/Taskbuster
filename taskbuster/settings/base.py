4eacc49c99294ca3e1591d23638fb4db82f0a93b
Giovanni21M <froggyriseman@gmail.com>
2016-05-23 21:48:53 -0400

    Settings, static files and templates

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -57,10 +57,11 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'taskbuster.urls'
 
+# Template files
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
